8448795f7da8bd8c24cacc283480a74a2fe5a524
ychoi60 <47227656+ychoi60@users.noreply.github.com>
2019-02-25 15:38:49 -0500

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,9 @@
+﻿function imgFunc() {
+var bigImage = document.getElementById("bigImage");
+var thumbnailsHolder = document.getElementById("thumbnailsHolder");
+    thumbnailsHolder.addEventListener("click", function (event) {
+    if (event.target.tagName == "IMG") {
+                bigImage.src = event.target.src;
+                        }    }, false);
+                        }
+                        window.addEventListener("load", imgFunc, false);
---
+++ zoom.js
@@ -0,0 +1,13 @@
+﻿function view() {
+    imgsrc = "IMG1.jpg";
+    imgsrc = "IMG2.jpg";
+    imgsrc = "IMG3.jpg";
+    imgsrc = "IMG4.jpg";
+    
+        viewwin = window.open(imgsrc, 'viewwin', 'width=600,height=300');
+        }
+        
+        function closeWin() {
+            Window.close(); 
+            
+            }
